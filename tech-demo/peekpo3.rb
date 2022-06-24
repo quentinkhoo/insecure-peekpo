6e6dcd67f67a26272ef1a03f1e572c8674e09f80
Quentin Khoo <quentin.khoo@ascendaloyalty.com>
2022-06-24 18:45:54 +0800

    init

---
+++ tech-demo/peekpo3.rb
@@ -0,0 +1,65 @@
+#Nice Try!:)
+require 'eventmachine'
+
+module Peekpo
+  def post_init
+    set_comm_inactivity_timeout(1000000000)
+    send_data "⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁⠀⠉⠙⠛⠿⠛⠉⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⣿⣿⣿⡿⠋⠀⢀⠔⠒⠒⠒⠒⠀⠠⠔⠚⠉⠉⠁⠀⠙⢿⣿⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⣿⠟⢋⠀⠀⠀⠀⣠⢔⠤⠀⢤⣤⡀⠠⣢⡭⠋⡙⢿⣭⡨⠻⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⠇⢀⠆⠀⠀⠀⣪⣴⣿⠐⢬⠀⣿⡗⣾⣿⡇⠈⠦⢸⣿⠗⢠⠿⠿⣿⣿\n"
+    send_data "⣿⣿⡏⠀⠀⠀⢀⡀⠀⠈⠛⠻⠄⠀⠠⠋⠀⠈⠛⠻⠆⠀⠈⢀⡠⣬⣠⢣⣶⢸\n"
+    send_data "⣿⡿⠀⠀⠀⣶⡌⣇⣿⢰⠷⠦⠄⣀⣀⣀⣀⣀⣀⣠⣤⠶⠞⡛⠁⣿⣿⣾⣱⢸\n"
+    send_data "⣿⡇⠀⠀⠀⣬⣽⣿⣿⢸⡜⢿⣷⣶⣶⣶⣯⣽⣦⡲⣾⣿⡇⣿⠀⣌⠿⣿⠏⣼\n"
+    send_data "⣿⡇⠀⠀⠀⠹⣿⡿⢫⡈⠻⢦⡹⢟⣛⣛⣛⣛⣛⣡⠿⣫⡼⠃⠀⣿⡷⢠⣾⣿\n"
+    send_data "⣿⡇⡀⠀⠀⠀⠀⠰⣿⣷⡀⠀⠙⠳⠦⣭⣭⣭⣵⡶⠿⠋⠀⢀⣴⣿⡇⣾⣿⣿\n"
+    send_data "⣿⢠⣿⣦⣄⣀⠀⠀⢻⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣀⣤⣶⣿⣿⡟⣰⣿⣿\n"
+    send_data "⡇⣸⣿⣿⣿⣿⣿⣷⣦⢹⣿⣇⢠⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⡇⢹⣿⣿⣿\n"
+    send_data "⡇⣿⣿⣿⣿⣿⣿⣿⣿⣎⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⣸⣿⣿⣿\n"
+    send_data "⣧⡘⠿⢿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢋⣡⣾⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⣶⣤⣍⣉⣛⣛⡛⠛⠛⢛⣛⣛⣛⣛⣉⣩⣭⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿\n"
+    send_data "Hello! I am Peekpo. I was made with Ruby. I peek into files.\n"
+    send_data "For example, I can peek into apu.txt, peepo.txt, but not flag.txt becuz Peekpo hate flags\n"
+    send_data "Tell me a file and I'll peek into it:\n"
+  end
+
+  def receive_data data
+    begin
+      data = data.strip 
+      if data.length < 3
+        send_data "Peekpo needs more to understand!\n"
+      else
+        if data.length > 25
+          send_data "Information overload! Peekpo cannot process so much!\n"
+        else
+          if data =~ /help/
+            send_data "You asked for help? Peekpo helps!\n"
+            send_data "Peekpo shows you how peekpo peeks:\n"
+            send_data "`head -c 12 \#{data}`\n"
+          else
+            if check_blacklist(data)
+              send_data "Peekpo cannot understand you. :(\n"
+              send_data "Maybe ask Peekpo for help?\n"
+            else
+              user_input = `head -c 12 #{data} 2>&1`
+              send_data "This is your result: #{user_input}\n"
+            end
+          end
+        end
+      end
+    rescue Exception => e
+      send_data e.message + "\n"
+      send_data e.backtrace.join("\n")+"\n"
+    end
+  end
+
+  def check_blacklist str
+    str =~ /[\s;\${}]/
+  end
+end
+
+
+EventMachine.run {
+  EventMachine.start_server "", 29900, Peekpo
+  puts "Peekpo started!"
+}
---
+++ tech-demo/peekpo_brakeman_fails.rb
@@ -0,0 +1,63 @@
+#Nice Try!:)
+require 'eventmachine'
+require 'open3'
+
+module Peekpo
+  def post_init
+    set_comm_inactivity_timeout(10)
+    send_data "⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁⠀⠉⠙⠛⠿⠛⠉⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⣿⣿⣿⡿⠋⠀⢀⠔⠒⠒⠒⠒⠀⠠⠔⠚⠉⠉⠁⠀⠙⢿⣿⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⣿⠟⢋⠀⠀⠀⠀⣠⢔⠤⠀⢤⣤⡀⠠⣢⡭⠋⡙⢿⣭⡨⠻⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⠇⢀⠆⠀⠀⠀⣪⣴⣿⠐⢬⠀⣿⡗⣾⣿⡇⠈⠦⢸⣿⠗⢠⠿⠿⣿⣿\n"
+    send_data "⣿⣿⡏⠀⠀⠀⢀⡀⠀⠈⠛⠻⠄⠀⠠⠋⠀⠈⠛⠻⠆⠀⠈⢀⡠⣬⣠⢣⣶⢸\n"
+    send_data "⣿⡿⠀⠀⠀⣶⡌⣇⣿⢰⠷⠦⠄⣀⣀⣀⣀⣀⣀⣠⣤⠶⠞⡛⠁⣿⣿⣾⣱⢸\n"
+    send_data "⣿⡇⠀⠀⠀⣬⣽⣿⣿⢸⡜⢿⣷⣶⣶⣶⣯⣽⣦⡲⣾⣿⡇⣿⠀⣌⠿⣿⠏⣼\n"
+    send_data "⣿⡇⠀⠀⠀⠹⣿⡿⢫⡈⠻⢦⡹⢟⣛⣛⣛⣛⣛⣡⠿⣫⡼⠃⠀⣿⡷⢠⣾⣿\n"
+    send_data "⣿⡇⡀⠀⠀⠀⠀⠰⣿⣷⡀⠀⠙⠳⠦⣭⣭⣭⣵⡶⠿⠋⠀⢀⣴⣿⡇⣾⣿⣿\n"
+    send_data "⣿⢠⣿⣦⣄⣀⠀⠀⢻⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⣀⣤⣶⣿⣿⡟⣰⣿⣿\n"
+    send_data "⡇⣸⣿⣿⣿⣿⣿⣷⣦⢹⣿⣇⢠⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⡇⢹⣿⣿⣿\n"
+    send_data "⡇⣿⣿⣿⣿⣿⣿⣿⣿⣎⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⣸⣿⣿⣿\n"
+    send_data "⣧⡘⠿⢿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢋⣡⣾⣿⣿⣿⣿\n"
+    send_data "⣿⣿⣿⣶⣤⣍⣉⣛⣛⡛⠛⠛⢛⣛⣛⣛⣛⣉⣩⣭⣤⣴⣶⣿⣿⣿⣿⣿⣿⣿\n"
+    send_data "Hello! I am Peekpo. I was made with Ruby. I peek into files. Tell me a file and I'll peek into it: "
+  end
+
+  def receive_data data
+    begin
+      data = data.strip 
+      if data.length < 3
+        send_data "Peekpo needs more to understand!\n"
+      else
+        if data.length > 500 
+          send_data "Information overload! Peekpo cannot process so much!\n"
+        else
+          if data =~ /help/
+            send_data "You asked for help? Peekpo helps!\n"
+            send_data "Peekpo shows you how peekpo peeks:\n"
+            send_data "`head -c 12 \"\#{data}\"`\n"
+          else
+            Open3.popen3(data) do |stdin, stdout, stderr, wait_thread|
+              err = stderr.gets(nil)
+              out = stdout.gets(nil)
+              [stdin, stdout, stderr].each{|stream| stream.send('close')}
+              exit_status = wait_thread.value
+            end
+          end
+        end
+      end
+    rescue Exception => e
+      send_data e.message + "\n"
+      send_data e.backtrace.join("\n")+"\n"
+    end
+  end
+
+  def check_blacklist str
+    str =~ /[\d\s\(\)<>\?\+\/\\\!`:@\*#\^\%]/
+  end
+end
+
+
+EventMachine.run {
+  EventMachine.start_server "", 29900, Peekpo
+  puts "Peekpo started!"
+}
